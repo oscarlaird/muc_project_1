a144ffee57be06907a3dc586cba2d72f1e858d45
Kevin Key <key@gatech.edu>
2021-02-15 09:55:46 -0500

    files

---
+++ task5.py
@@ -0,0 +1,9 @@
+""" Task 5
+Write the code for your task 5 in this file. You can use any method
+you want. You can consider using the Bulling work flow. If you want,
+you can use machine learning to solve the problem. Scikit learn is a
+good python library for machine learning. You can also use the gui
+interface of weka. If you do that, write your feature extraction
+in this python file and describe how you used the weka gui to build
+the model.
+"""
