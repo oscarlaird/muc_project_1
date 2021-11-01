7408819ff45337c9940a5adafc02e9947cb12a20
Michael Verges <maustinstar@gmail.com>
2021-11-01 14:47:48 -0400

    Remove walking_steps_2 & Change point distribution

--- task2.py
+++ task2.py
@@ -8,7 +8,7 @@ def clean_data(data):
 
 
     print ("Create new file without garbage data and save it in data folder")
-    file_name_clean = "walking_steps_1_clean.csv"
+    file_name_clean = "walking_steps_clean.csv"
 
 
 
@@ -16,7 +16,7 @@ def clean_data(data):
 
 def main():
     # Get data
-    file_name = "walking_steps_1.csv"  # Change to your file name
+    file_name = "walking_steps.csv"  # Change to your file name
     data = parser_data.get_data(file_name) #data -- time,X,Y,Z
     clean_data(data)
 
